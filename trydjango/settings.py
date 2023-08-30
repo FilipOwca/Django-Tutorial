9562b8181a15bca78c7ad60339cef3ae5aa57e78
filip.owca <owcafilip@gmail.com>
2023-08-30 23:44:22 +0200

    After video 38

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', 'django-insecure-325o4+*#9owo_&^1gr_2r#1er@@e&i8a7dp-c3gq9i@p)=8!r)')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 == True
